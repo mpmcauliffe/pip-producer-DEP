658112df3d7077d5ce90c65a84c44357d9431968
mpmcauliffe <mcauliffe99@gmail.com>
2019-08-08 18:57:35 -0600

    web server, connection to mongoDB

---
+++ server.js
@@ -0,0 +1,48 @@
+const express       = require('express')
+const mongoose      = require('mongoose')
+const bodyParser    = require('body-parser')
+const path          = require('path')
+
+const connectDB     = require('./config/db')
+// const auth          = require('./routes/api/auth')
+// const users         = require('./routes/api/users')
+// const articles      = require('./routes/api/articles')
+// const upload        = require('./routes/api/upload')
+// const admin         = require('./routes/api/admin')
+
+const app           = express()
+const PORT          = process.env.PORT || 5000
+
+
+connectDB()
+
+
+//app.use(bodyParser.json())
+app.use(express.json({ extended: false }))
+
+app.get('/', (req, res) => {
+    res.json({ msg: 'welcome to the pip-producer'})
+})
+
+
+/* API */
+// app.use('/api/auth', auth)
+// app.use('/api/users', users)
+// app.use('/api/articles', articles)
+// app.use('/api/upload', upload)
+// app.use('/api/admin', admin)
+
+// // AWS Ubuntu 18.04 AND OTHERS
+// app.use(express.static(path.join(__dirname, 'client', 'build')))
+
+// // Heroku
+// /*** serve static assets in production ***/
+// if (process.env.NODE_ENV === 'production') {
+//     //app.use(express.static('client/build'))
+
+//     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+// }
+
+
+/* LISTENING */
+app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`))
